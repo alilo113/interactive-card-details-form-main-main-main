f9e96102b6355bffbd4388e6925990a918d87e80
alilo113 <65548875+alilo113@users.noreply.github.com>
2022-09-11 14:31:34 +0100

    Update form.js

--- alilo113-interactive-card-details-form-main-main-main/interactive-card-details-form-main/form.js
+++ alilo113-interactive-card-details-form-main-main-main/interactive-card-details-form-main/form.js
@@ -3,32 +3,54 @@ const cardHolderName = document.getElementById("cardHolderName");
 const cardHolerNameError = document.getElementById("cardHolerNameError");
 const cardNum = document.getElementById("cardNum");
 const cardHolderNumEror = document.getElementById("cardHolderNumEror");
+const mounth = document.getElementById("mounth");
+const year = document.getElementById("year");
+const Error = document.getElementById("Error");
 
 form.addEventListener("submit",(e) => {
 
     if(cardHolderName.value === ""){
-        cardHolerNameError.innerText = "can't be blank";
+        cardHolerNameError.innerHTML = "can't be blank";
         cardHolderName.style.borderColor = "red";
         e.preventDefault();
-    }
-    else if(cardHolderName.value !== ""){
-        console.log(cardHolderName.value);
-        cardHolderName.style.borderColor = "green"
+
+    }else{
+        cardHolderName.style.borderColor = "green";
         e.preventDefault();
         cardHolerNameError.innerHTML = "";
     }
 
-    let notCardNum = "9591 " + "6489 " + "6389 " + "101E";
-    let notNum = cardNum.value
-
-    if(cardNum.value === notCardNum || cardNum.value === notNum.toString()){
-        cardHolderNumEror.innerHTML = "wrond format, numbers only";
+    if(cardNum.value === ""){
+        cardHolderNumEror.innerHTML = "can't be blank";
         cardNum.style.borderColor = "red";
+
+    }else{
+        cardNum.style.borderColor = "green";
+        cardHolderNumEror.innerHTML = "";
     }
-    else if(cardNum.value === ""){
-        cardHolderNumEror.innerHTML = "can't be blank"; 
+
+    let notCardNumber = "9591 " +  " 6489 " + " 6389 " + " 101E";
+    let ifStr =  cardNum.value;
+
+    if(cardNum.value === notCardNumber || ifStr != NaN){
+        cardHolderNumEror.innerHTML = "numbers only";
+        cardNum.style.borderColor = "red";
+
+    }else{
+        cardNum.style.borderColor = "green";
+        cardHolderNumEror.innerHTML = "";
+    }
+
+    if(mounth.value === ""){
+        Error.innerHTML = "Can't be blank";
+        mounth.style.borderColor = "red";
+
+    }else if(year.value === ""){
+        Error.innerHTML = "Can't be blank";
+        year.style.borderColor = "red";
     }
     else {
-        cardHolderNumEror.borderColor = "green";
+        mounth.style.borderColor = "green";
+        year.style.borderColor = "green";
     }
 })
